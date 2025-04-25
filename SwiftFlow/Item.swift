803ac46498c8c32830ab36d32cc308e2874fd6c9
Kevin Perez <kp952547@gmail.com>
2025-04-24 21:01:17 -0500

    Initial Commit

---
+++ SwiftFlow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftFlow
+//
+//  Created by Kevin Perez on 4/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
